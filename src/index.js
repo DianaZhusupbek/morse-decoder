3b2e43492632725571f9fbe8c5841954dcc39143
Diana <diana.zhusupbekova.kk@gmail.com>
2022-02-25 01:41:18 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,22 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
-
-module.exports = {
-    decode
-}
+    let arraySymbols = [];
+    for (let i = 0; i < expr.length; i = i + 10) {
+      let symbolBinary = expr.substring(i, i + 10);
+      if (symbolBinary.includes('*')) {
+        arraySymbols.push(' ');
+        continue;
+      }
+      symbolBinary = symbolBinary.replace(/11/g, '-');
+      symbolBinary = symbolBinary.replace(/10/g, '.');
+      symbolBinary = symbolBinary.replace(/00/g, '');
+      arraySymbols.push(MORSE_TABLE[symbolBinary]);
+  
+    }
+    return arraySymbols.join('');
+  }
+  
+  module.exports = {
+    decode,
+  };
